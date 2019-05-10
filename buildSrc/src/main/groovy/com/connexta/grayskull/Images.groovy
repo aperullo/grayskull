e70c0afdff8e10e396d0b437ac7fc39b0e5d0b70
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2019-05-10 16:10:06 -0400

    Adding several stacks

---
+++ buildSrc/src/main/groovy/com/connexta/grayskull/Images.groovy
@@ -0,0 +1,12 @@
+package com.connexta.grayskull
+
+class Images {
+    static final String proxyImage = 'traefik'
+    static final String proxyVersion = '1.7'
+    static final String portainerImage = 'portainer/portainer'
+    static final String portainerAgentImage = 'portainer/agent'
+    static final String registryImage = 'registry'
+    static final String registryVersion = '2.5.2'
+    static final String registryUiImage = 'reg'
+    static final String registryUiVersion = 'v0.16.0'
+}
---
+++ buildSrc/src/main/groovy/com/connexta/grayskull/Platform.groovy
@@ -0,0 +1,8 @@
+package com.connexta.grayskull
+
+class Platform {
+    static final String composeSyntaxVersion = '3.7'
+    static final String monitoringNetwork = 'monitoring'
+    static final String publicNetwork = 'public'
+    static final String securityNetwork = 'security'
+}
