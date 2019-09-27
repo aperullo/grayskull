c543e006e65243e29d7b65744122b4a98f2cce82
aperullo <aperullo065@gmail.com>
2019-09-27 12:02:49 -0400

    address pr changes

--- swarm/terraform/swarm_cluster/security-groups.tf
+++ swarm/terraform/swarm_cluster/security-groups.tf
@@ -15,3 +15,29 @@ resource "aws_security_group" "swarm_ports" {
   }
 
 }
+
+# TODO: Make this into a list so the registry can talk to eachother.
+resource "aws_security_group" "swarm_to_reg" {
+  name        = "swarm_ports"
+  description = "Allow swarm cluster communication"
+  vpc_id      = "${var.vpc_id}"
+
+  # TODO: Change to allow between specific hosts only
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "${formatlist(
+                      "%s/32", 
+                      "${concat(
+                        "${aws_instance.swarm_master[*].public_ip}",
+                        "${aws_instance.swarm_worker[*].public_ip}"
+                        )
+                      }"
+                    )}" # add a CIDR block here
+  }
+
+}
