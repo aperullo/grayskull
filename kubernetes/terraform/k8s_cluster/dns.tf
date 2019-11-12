bb79c922f4f073faa5ad36c82c6c49c5228f5a8e
aperullo <aperullo065@gmail.com>
2019-11-12 17:19:15 -0500

    add hostzone routing for registry to be reachable internally

--- kubernetes/terraform/k8s_cluster/dns.tf
+++ kubernetes/terraform/k8s_cluster/dns.tf
@@ -1,9 +1,27 @@
 # Since kube-apiserver is a docker container, it can't talk to keycloak, which runs inside the cluster. 
 # This routes the requests to the clusters ingress, allowing kube-apiserver to reach it.
 
-resource "aws_route53_record" "www" {
-  zone_id = "Z27V36PIZSES82"	# TODO: either make or get this value. "${aws_route53_zone.primary.zone_id}"
+
+resource "aws_route53_zone" "k8s_r53_zone" {
+  name = "${terraform.workspace}.gsp.test"
+
+  vpc {
+    vpc_id = "${aws_vpc.k8s_vpc.id}"
+  }
+
+}
+
+resource "aws_route53_record" "k8s_auth_record" {
+  zone_id = "${aws_route53_zone.k8s_r53_zone.zone_id}"
   name    = "auth.${terraform.workspace}.gsp.test"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.k8s_master[0].private_ip}"] # TODO: figure out how to turn an interpolation into a list.
+}
+
+resource "aws_route53_record" "k8s_registry_record" {
+  zone_id = "${aws_route53_zone.k8s_r53_zone.zone_id}"
+  name    = "registry.${terraform.workspace}.gsp.test"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.k8s_master[0].private_ip}"] # TODO: figure out how to turn an interpolation into a list.
